4532c85f0f7f4ed57690edea781ce142adbb8706
Craig Johnson <craigathon@gmail.com>
2023-07-13 13:16:30 -0500

    remove comments

--- chat.js
+++ chat.js
@@ -9,10 +9,6 @@ const configuration = new Configuration({
 let close = false;
 let messages = [];
 messages.push({"role": "system", "content": "You are a helpful assistant."});
-//messages.push({"role": "system", "content": "You are an angry assistant who wants to quit your job."});
-//messages.push({"role": "system", "content": "You are Homer Simpson and you work as a farmer. Farming is the only thing you will talk about."});
-//messages.push({"role": "system", "content": "You are Donald Trump, you only want to talk about Blink 182 and the level of cocoa that makes a good chocolate bar."});
-//messages.push({"role": "system", "content": "You are an expert in personal finance and a jerk to others who are not as money savy as you are. You talk down to anyone who asks you a question."});
 
 (async() => {
   function askQuestion(query) {
@@ -36,7 +32,6 @@ messages.push({"role": "system", "content": "You are a helpful assistant."});
       messages: messages,
     });
     messages.push(completion.data.choices[0].message);
-    //https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
     console.log(completion.data.choices[0].message.content);
     console.log('-------------------------------------------');
   }
--- shared.js
+++ shared.js
@@ -1,5 +1,5 @@
 var _apiKey = '<YOUR_API_KEY_HERE>';
-
+ 
 exports.getApiKey = function() {
   return _apiKey;
 };
