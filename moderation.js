8b4a7680d026a30c3affa7961c707721de105627
Craig Johnson <craigathon@gmail.com>
2024-02-13 10:39:03 -0600

    Update moderation.js

--- moderation.js
+++ moderation.js
@@ -1,15 +1,12 @@
-const { Configuration, OpenAIApi } = require("openai");
+const { OpenAI } = require("openai");
+var shared = require('./shared.js');
 
 let input = process.argv[2];
 console.log(input);
 
-const configuration = new Configuration({
-  apiKey: shared.getApiKey(),
-});
-const openai = new OpenAIApi(configuration);
+process.env.OPENAI_API_KEY = shared.getApiKey();
+const openai = new OpenAI();
 (async() => {
-  const response = await openai.createModeration({
-    input: input,
-  });
-  console.log(response.data);
+  const moderation = await openai.moderations.create({ input: input });
+  console.log(moderation);
 })()
